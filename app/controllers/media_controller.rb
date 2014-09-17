c0febc33c2123e6da1a3015f8c81fb7fd6da287c
dave <dave@instructure.com>
2014-09-16 18:19:23 -0600

    remove photos folder

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -4,12 +4,6 @@ class MediaController < ApplicationController
 
   def index
     @albums = JSON.parse(ENV['ALBUMS'])
-    @photos = []
-    Dir.foreach("#{Rails.root}/app/assets/images/photos") do |item|
-      next if item == '.' || item == '..' || item == '.DS_Store'
-      thumb = "#{item.split(".jpg").first}_thumb.jpg"
-      @photos << {picture: item, thumb: thumb}
-    end
   end
 
   def albums
